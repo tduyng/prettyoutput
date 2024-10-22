10c6c94a3fc2a82d6b3443a9f7dc4615babb7c6f
tduyng <tduyng.dev@gmail.com>
2024-10-22 19:59:36 +0200

    feat(2.0): implement biome, remove eslint and prettier

--- benchmark/stats.js
+++ benchmark/stats.js
@@ -1,48 +1,51 @@
-'use strict'
-
 const _ = require('lodash')
 
-const fixedInt = (exports.fixedInt = function(v) {
-    return Math.floor(v)
-})
+// biome-ignore lint/suspicious/noAssignInExpressions: <explanation>
+const fixedInt = (exports.fixedInt = (v) => Math.floor(v))
 
 /**
  *
  * @param {number} time - in nano seconds
  */
-exports.prettyTime = function(time) {
+exports.prettyTime = (time) => {
     const mn = fixedInt(time / 1e12)
     const s = fixedInt(time / 1e9) - fixedInt(mn * 1e3)
     const ms = fixedInt(time / 1e6) - fixedInt(mn * 1e6) - fixedInt(s * 1e3)
-    const micros = fixedInt(time / 1e3) - fixedInt(mn * 1e9) - fixedInt(s * 1e6) - fixedInt(ms * 1e3)
-    const ns = fixedInt(time) - fixedInt(mn * 1e12) - fixedInt(s * 1e9) - fixedInt(ms * 1e6) - fixedInt(micros * 1e3)
+    const micros =
+        fixedInt(time / 1e3) - fixedInt(mn * 1e9) - fixedInt(s * 1e6) - fixedInt(ms * 1e3)
+    const ns =
+        fixedInt(time) -
+        fixedInt(mn * 1e12) -
+        fixedInt(s * 1e9) -
+        fixedInt(ms * 1e6) -
+        fixedInt(micros * 1e3)
 
     let result = ''
-    if (mn != 0) result += ` ${mn} mn`
-    if (s != 0) result += ` ${s} s`
-    if (ms != 0) result += ` ${ms} ms`
-    if (micros != 0) result += ` ${micros} µs`
-    if (ns != 0) result += ` ${ns} ns`
+    if (mn !== 0) result += ` ${mn} mn`
+    if (s !== 0) result += ` ${s} s`
+    if (ms !== 0) result += ` ${ms} ms`
+    if (micros !== 0) result += ` ${micros} µs`
+    if (ns !== 0) result += ` ${ns} ns`
 
     return result
 }
 
-exports.stats = function(diffs) {
+exports.stats = (diffs) => {
     let min = diffs[0]
     let max = diffs[0]
     let total = 0
 
-    _.forEach(diffs, diff => {
+    _.forEach(diffs, (diff) => {
         min = Math.min(min, diff)
         max = Math.max(max, diff)
         total += diff
     })
 
-    let mean = total / diffs.length
+    const mean = total / diffs.length
     return { min, max, mean, total }
 }
 
-exports.prettyStats = function(stats) {
+exports.prettyStats = (stats) => {
     const result = {}
     result.min = exports.prettyTime(stats.min)
     result.max = exports.prettyTime(stats.max)
--- examples/example.js
+++ examples/example.js
@@ -1,11 +1,9 @@
 const prettyoutput = require('../lib/prettyoutput')
 
-/* eslint-disable no-console */
-
 const data = {
     username: 'kic',
     url: 'https://github.com/keepitcool',
-    projects: ['prettyoutput', '3m2tuio']
+    projects: ['prettyoutput', '3m2tuio'],
 }
 
 console.log(prettyoutput(data))
