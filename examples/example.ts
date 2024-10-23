1a87b15805b8ab6d1f28a30a3f9bb64189c97beb
tduyng <tduyng.dev@gmail.com>
2024-10-23 22:02:12 +0200

    chore(2.0): complete more informations for examples

--- examples/example.ts
+++ examples/example.ts
@@ -5,6 +5,38 @@ const data = {
     url: 'https://github.com/keepitcool',
     projects: ['prettyoutput', '3m2tuio'],
     age: 18,
+    details: {
+        hobbies: ['coding', 'gaming', 'reading'],
+        education: {
+            degree: 'Bachelor of Science',
+            major: 'Computer Science',
+            graduationYear: 2022,
+        },
+        workExperience: [
+            {
+                company: 'TechCorp',
+                position: 'Software Engineer',
+                years: 2,
+            },
+            {
+                company: 'WebSolutions',
+                position: 'Frontend Developer',
+                years: 1,
+            },
+        ],
+    },
+    isActive: true,
+    contact: {
+        email: 'kic@example.com',
+        phone: {
+            home: '123-456-7890',
+            work: '098-765-4321',
+        },
+    },
+    socialMedia: {
+        twitter: '@kic',
+        linkedIn: 'linkedin.com/in/kic',
+    },
 }
 
-console.log(prettyoutput(data, { maxDepth: 2, colors: { number: 'red' } }))
+console.log(prettyoutput(data, { maxDepth: 5, colors: { number: 'blue' } }))
